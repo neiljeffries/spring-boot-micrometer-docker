5e29702285b7d7c38769a2d38bb968caebcb14b2
Neil Jeffries <neiljeffries@gmail.com>
2025-09-14 15:33:00 -0400

    sending logs to Loki now working

--- src/main/java/com/neiljeffries/demo/MyController.java
+++ src/main/java/com/neiljeffries/demo/MyController.java
@@ -3,9 +3,11 @@ package com.neiljeffries.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class MyController {
 
     private final MyService testService;
@@ -16,6 +18,7 @@ public class MyController {
 
     @GetMapping
     public MyResponse test() {
+        log.info("Received request at /test endpoint");
         return testService.getTestMessage();
     }
 }
